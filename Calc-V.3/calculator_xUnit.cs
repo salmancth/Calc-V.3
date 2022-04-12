936cad3e0365b498d82500d3f55ec75a117fdc72
Salman Yahya <33560460+salmancth@users.noreply.github.com>
2022-04-12 09:10:17 +0200

    Add files via upload

---
+++ Calc-V.3/calculator_xUnit.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_xUnit
+{
+    public class Calculator_X
+    {
+        double Add;
+        double Minus;
+        double Mult;
+        double Div;
+        double Answer;
+
+        double number1;
+        double number2;
+
+        double usernumber1;
+        double usernumber2;
+
+        util = new Utils();
+
+
+        userNumber1 = utils.ConvertToDouble(number1);
+        userNumber2 = utils.ConvertToDouble(number2);
+
+
+        
+        public void Add()
+        {
+
+            try
+            {
+                Console.WriteLine("Enter two number to add");
+                Console.WriteLine("First number:")
+                number1 = Console.ReadLine();
+                
+                Console.WriteLine("Second number: ");
+                number2 = Console.ReadLine();
+                
+                Answer = utils.AddUp(userNumber1, userNumber2);
+                utils.Result(Answer);
+            }
+            catch (Exception ex)
+            {
+                utils.CatchWarrning();
+            };
+
+        }
+// --------------------------------------------------------------------
+        public void Minus()
+        {
+
+            try
+            {
+                Console.WriteLine("Enter two number to substruct");
+                Console.WriteLine("First number:")
+                number1 = Console.ReadLine();
+                
+                Console.WriteLine("Second number: ");
+                number2 = Console.ReadLine();
+                
+                Answer = utils.Minus(userNumber1, userNumber2);
+                utils.Result(Answer);
+            }
+            catch (Exception ex)
+            {
+                utils.CatchWarrning();
+            };
+
+// -------------------------------------------------------------------------
+
+        public void Mult()
+        {
+
+            try
+            {
+                    Console.WriteLine("Enter two number to Multiply");
+                    Console.WriteLine("First number:")
+                    number1 = Console.ReadLine();
+
+                    Console.WriteLine("Second number: ");
+                    number2 = Console.ReadLine();
+
+                    Answer = utils.Mult(userNumber1, userNumber2);
+                    utils.Result(Answer);
+                }
+                catch (Exception ex)
+                {
+                    utils.CatchWarrning();
+                };
+
+//--------------------------------------------------------------------------------
+            public void Div()
+            {
+
+                try
+                
+                {
+                    Console.WriteLine("Enter two number to Devide");
+                    Console.WriteLine("First number:")
+                    number1 = Console.ReadLine();
+                
+                    Console.WriteLine("Second number: ");
+                    number2 = Console.ReadLine();
+                
+                    Answer = utils.Div(userNumber1, userNumber2);
+                    utils.Result(Answer);
+                }
+                catch (Exception ex)
+                {
+                    utils.CatchWarrning();
+                };
+
+            
+            }
+
+        }
+
+    }
+}
